5d71ccb67fee80748bf75ed4e9a3fe502285d4db
Will Montiel <willmontiel@Wills-MacBook-Pro.local>
2020-11-17 15:47:10 -0500

    Test panTo

--- src/single-context-menu.js
+++ src/single-context-menu.js
@@ -73,7 +73,7 @@ export default class SingleContextMenu {
 
         this._overlay.setMap(this._context._map);
         this.displayed = true;
-        this._context._map.panTo(this.position);
+        //this._context._map.panTo(this.position);
     }
 
     hide() {
